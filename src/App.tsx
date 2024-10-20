4c2a339225865f03b83c4dadb246091b0148db5f
RyamL1221 <ryaml1221@gmail.com>
2024-10-20 15:25:56 -0400

    functionality to rotate chores

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [jacobAndRyan, setJacobAndRyan] = useState(0);
+  const [raymondAndRohan, setRaymondAndRohan] = useState(1);
+  const [bradyAndJack, setBradyAndJack] = useState(2);
+
+  const chores = ["Bathroom cleaning (toilet & shower)", "Sink area cleaning", "Vacuum/Dusting/Tidying common room"];
+
+  const rotateChores = () => {
+    setJacobAndRyan((jacobAndRyan + 1) % chores.length);
+    setRaymondAndRohan((raymondAndRohan + 1) % chores.length);
+    setBradyAndJack((bradyAndJack + 1) % chores.length);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jacob and Ryan: {chores[jacobAndRyan]}</h1>
+      <h1>Raymond and Rohan: {chores[raymondAndRohan]}</h1>
+      <h1>Brady and Jack: {chores[bradyAndJack]}</h1>
+      <button id="rotate-chores-button" onClick={rotateChores}>Rotate Chores</button>
     </div>
   );
 }
